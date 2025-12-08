849441f18c9c72746fc53d2a9d814cc13bccd4cc
Cesar Augusto <cesarprofissional22@gmail.com>
2025-12-07 21:43:13 -0300

    Sintaxe Básica

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using TIVIT___.Net_com_GitHub_Copilot.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Cesar";
+pessoa1.Idade = 22;
+pessoa1.Endereco = "São Paulo";
+pessoa1.Apresentar();  
