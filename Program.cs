cb608c8aa256bc00d2f022b5043fd9039506c088
Cesar Augusto <cesarprofissional22@gmail.com>
2025-12-08 22:53:52 -0300

    tipos de variaveis2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿using TIVIT___.Net_com_GitHub_Copilot.Models;
 
 string fala = "Ola, seja bem vindo";
+int quantidade = 1;
+double altura = 1.82;
+decimal preco = 1.80M;
+bool condicao = true;
 
-
-
-
-
-
-
+Console.WriteLine(fala);
+Console.WriteLine("Valor da variavel quantidade: " + quantidade);
+Console.WriteLine("Valor da variavel altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variavel preco: " + preco);
+Console.WriteLine("Valor da variavel condicao: " + condicao);
 
 
 Pessoa pessoa1 = new Pessoa();
